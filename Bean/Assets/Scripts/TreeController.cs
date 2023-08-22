b381df3482ef7409d94d27a0608978307066f6bd
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2023-08-22 08:45:48 -0600

    dog

--- Bean/Assets/Scripts/TreeController.cs
+++ Bean/Assets/Scripts/TreeController.cs
@@ -5,21 +5,39 @@ using UnityEngine.UIElements;
 
 public class TreeController : MonoBehaviour
 {
-    float lifeTime = 0f;
+    public float lifeTime = 0f;
+    public int treeState = 3;
     void Update()
     {
         lifeTime += Time.deltaTime;
+        if(lifeTime > 10)
+        {
+            treeState = 1;
+            gameObject.transform.localScale = new Vector3(1, 1, 1) / 2;
+        }else if(lifeTime > 20)
+        {
+            treeState = 2;
+            gameObject.transform.localScale = new Vector3(1, 1, 1) / 1.5f;
+        }else if(lifeTime > 30)
+        {
+            treeState = 3;
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            treeState = 0;
+            gameObject.transform.localScale = new Vector3();
+        }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.tag);
         if (other.tag == "Player")
         {
-            Debug.Log("Hi");
-            other.gameObject.GetComponent<PlayerInv>().backPack["Tree Beans"] += 1;
+            if (treeState >= 3) other.gameObject.GetComponent<PlayerInv>().backPack["Tree Beans"] += 1;
             lifeTime = 0f;
+            treeState = 0;
         }
     }
 }
