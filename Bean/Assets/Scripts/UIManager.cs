d5c8521baede0c734e5c261258e523c7dad1bc75
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2023-09-01 14:10:35 -0600

    piss

--- Bean/Assets/Scripts/UIManager.cs
+++ Bean/Assets/Scripts/UIManager.cs
@@ -8,15 +8,33 @@ public class UIManager : MonoBehaviour
     public GameObject player;
     public GameObject cashTextObj;
     public TextMeshProUGUI cashText;
+    GameObject[] buttons;
     // Start is called before the first frame update
     void Start()
     {
-
+        buttons = GameObject.FindGameObjectsWithTag("Button");
     }
 
     // Update is called once per frame
     void Update()
     {
         cashText.text = Mathf.Round((float)player.GetComponent<PlayerInv>().cash).ToString();
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            setButtons(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            setButtons(false);
+        }
+    }
+
+    public void setButtons(bool set)
+    {
+        foreach (var obj in buttons)
+        {
+            obj.SetActive(set);
+        }
     }
 }
