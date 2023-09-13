47db1c200f5981edc6ffd82b7c4abd4a61bcb33d
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2023-09-13 10:04:06 -0600

    hhhh

--- Bean/Assets/Scripts/MineController.cs
+++ Bean/Assets/Scripts/MineController.cs
@@ -1,48 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class MineController : MonoBehaviour
 {
     public GameObject rock;
-    public int[,,] map = new int[100, 200, 200]; //[layer,x,y]
-    public GameObject[,,] rocks;
-    int mapOffset = 100;
+    public int[,,] map = new int[100, 100, 100]; //[layer,x,y]
+    public GameObject[,,] rocks = new GameObject[100,100,100];
+    int mapOffset = 50;
     // Start is called before the first frame update
     void Start()
     {
-        rocks[0, 0, 0] = new GameObject();
-        for(int i = 0; i < 100; i++)
-        {
-            makeLayer(i);
-        }
-        setLayer(0);
+        makeLayer(0);
     }
 
     // Update is called once per frame
 
     void makeLayer(float depth)
     {
-        for(int i = 0; i < 200; i++)
+        //GameObject[,] hhhh;
+        for (int i = 0; i < 20; i++)
         {
-            for (int j = 0; j < 200; j++)
+            for (int j = 0; j < 20; j++)
             {
-                rocks[(int)depth, i + mapOffset, j + mapOffset] = Instantiate(rock, transform.position + new Vector3(i + mapOffset, depth, j + mapOffset), Quaternion.identity);
-                
-                RockController hh = rocks[(int)depth, i + mapOffset, j + mapOffset].GetComponent<RockController>();
+                //rocks[(int)depth, i + mapOffset, j + mapOffset]
+                //hhhh[i, j] = hhh;
+
+                GameObject hhh = Instantiate(rock, transform.position + new Vector3(i, depth, j), Quaternion.identity);
+                //hhh.SetActive(true);
+                //rocks[(int)depth, i, j] = hhh;
+                RockController hh = hhh.GetComponent<RockController>();
                 hh.localPos = new Vector3(depth, i, j);
                 hh.mine = this;
-                
-                map[(int)depth, i + mapOffset, j + mapOffset] = 1;
+
+                map[(int)depth, i, j] = 1;
             }
         }
+        //rocks = plus(rocks,hhhh);
     }
 
     void setLayer(int depth)
     {
-        for (int i = 0; i < 200; i++)
+        for (int i = 0; i < 20; i++)
         {
-            for (int j = 0; j < 200; j++)
+            for (int j = 0; j < 20; j++)
             {
                 rocks[depth, i, j].SetActive(true);
             }
@@ -61,7 +63,7 @@ public class MineController : MonoBehaviour
             Debug.Log("Invalid Chords");
         }
         smartAdd((int)depth + 1, x, y);
-        smartAdd((int)depth -1, x, y);
+        smartAdd((int)depth - 1, x, y);
         smartAdd((int)depth, x + 1, y);
         smartAdd((int)depth, x - 1, y);
         smartAdd((int)depth, x, y + 1);
@@ -77,13 +79,14 @@ public class MineController : MonoBehaviour
         }
         try
         {
-            map[depth, x + mapOffset, y + mapOffset] = 1;
+            //map[depth, x + mapOffset, y + mapOffset] = 1;
 
-            /*GameObject ff = Instantiate(rock, transform.position + new Vector3(x, -depth, y), Quaternion.identity);
+            GameObject ff = Instantiate(rock, transform.position + new Vector3(x, -depth, y), Quaternion.identity);
+            //ff.SetActive(true);
             RockController f = ff.GetComponent<RockController>();
             f.localPos = new Vector3(depth, x ,y);
-            f.mine = this;*/
-            rocks[depth, x, y].SetActive(true);
+            f.mine = this;
+            //rocks[depth, x + mapOffset, y + mapOffset].SetActive(true);
         }
         catch
         {
